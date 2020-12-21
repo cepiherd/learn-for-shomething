48b5c2108bda5f0b3f5a8db1fa1978ad9fd4d95a
cepi herdiansyah <cepiherd@gmail.comgit>
2020-12-21 22:56:56 +0700

    cari tahu fungsi revert

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+
+const server = express()
+
+
+server.get('/' ,function(req , res){
+	res.json({
+		status : 200,
+		message : "You are Amazing"
+	})
+})
+
+
+server.listen(4000)
+
+
+console.log('Server Running on Port : 4000')
